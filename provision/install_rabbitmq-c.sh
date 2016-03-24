2fde9bae205034266e49104fb3a123f0826270d6
Thomas Vargiu <tvargiu@gmail.com>
2016-03-24 12:33:41 +0100

    Travis prova2

--- provision/install_rabbitmq-c.sh
+++ provision/install_rabbitmq-c.sh
@@ -5,7 +5,7 @@ set -e
 echo Installing rabbitmq-c ...
 
 
-#LIBRABBITMQ_VERSION=$1
+LIBRABBITMQ_VERSION=$1
 
 cd $HOME
 
@@ -22,23 +22,3 @@ git checkout ${LIBRABBITMQ_VERSION}
 
 git submodule init && git submodule update
 autoreconf -i && ./configure --prefix=$HOME/rabbitmq-c && make && make install
-
-
-echo Installing php-amqp ...
-
-
-#PHP_AMQP_VERSION=$1
-
-cd $HOME
-
-if [ ! -d "$HOME/php-amqp" ]; then
-  git clone git://github.com/pdezwart/php-amqp.git
-else
-  echo 'Using cached directory.';
-  cd $HOME/php-amqp
-  git fetch
-fi
-
-cd $HOME/php-amqp
-git checkout ${PHP_AMQP_VERSION}
-phpize && ./configure && make && sudo make install
